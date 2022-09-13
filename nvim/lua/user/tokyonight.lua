3b96e58cc339b413a54640ba628f829f94a323fc
Anthony Yarbrough <asyarb@hawaii.edu>
2022-09-13 09:58:32 -1000

    fix: tokyonight stuff

--- nvim/lua/user/tokyonight.lua
+++ nvim/lua/user/tokyonight.lua
@@ -4,3 +4,9 @@ require("tokyonight").setup({
 	dim_inactive = true,
 	transparent = true,
 })
+
+local colorscheme = "tokyonight"
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+	return
+end
