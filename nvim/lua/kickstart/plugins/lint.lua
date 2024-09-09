4f05cfc88dd31b3ca85bf747cbe10fbf4cb5974e
Anthony Yarbrough <asyarb@hawaii.edu>
2024-09-09 13:50:57 -1000

    biomejs

--- nvim/lua/kickstart/plugins/lint.lua
+++ nvim/lua/kickstart/plugins/lint.lua
@@ -1,46 +1,38 @@
-return {
+local function eslint_config_exists()
+  local eslint_files = {
+    '.eslintrc',
+    '.eslintrc.js',
+  }
+  local current_dir = vim.fn.getcwd()
 
+  for _, file in ipairs(eslint_files) do
+    if vim.fn.filereadable(vim.fn.glob(current_dir .. '/' .. file)) == 1 then
+      return true
+    end
+  end
+
+  return false
+end
+
+local function biome_config_exists()
+  return vim.fn.filereadable(vim.fn.glob(vim.fn.getcwd() .. '/biome.json')) == 1
+end
+
+local function get_linter()
+  if eslint_config_exists() then
+    return 'eslint_d'
+  elseif biome_config_exists() then
+    return 'biomejs'
+  end
+  return nil
+end
+
+return {
   { -- Linting
     'mfussenegger/nvim-lint',
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       local lint = require 'lint'
-      lint.linters_by_ft = {
-        typescript = { 'eslint_d' },
-        typescriptreact = { 'eslint_d' },
-      }
-
-      -- To allow other plugins to add linters to require('lint').linters_by_ft,
-      -- instead set linters_by_ft like this:
-      -- lint.linters_by_ft = lint.linters_by_ft or {}
-      -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
-      --
-      -- However, note that this will enable a set of default linters,
-      -- which will cause errors unless these tools are available:
-      -- {
-      --   clojure = { "clj-kondo" },
-      --   dockerfile = { "hadolint" },
-      --   inko = { "inko" },
-      --   janet = { "janet" },
-      --   json = { "jsonlint" },
-      --   markdown = { "vale" },
-      --   rst = { "vale" },
-      --   ruby = { "ruby" },
-      --   terraform = { "tflint" },
-      --   text = { "vale" }
-      -- }
-      --
-      -- You can disable the default linters by setting their filetypes to nil:
-      -- lint.linters_by_ft['clojure'] = nil
-      -- lint.linters_by_ft['dockerfile'] = nil
-      -- lint.linters_by_ft['inko'] = nil
-      -- lint.linters_by_ft['janet'] = nil
-      -- lint.linters_by_ft['json'] = nil
-      -- lint.linters_by_ft['markdown'] = nil
-      -- lint.linters_by_ft['rst'] = nil
-      -- lint.linters_by_ft['ruby'] = nil
-      -- lint.linters_by_ft['terraform'] = nil
-      -- lint.linters_by_ft['text'] = nil
 
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
@@ -48,7 +40,16 @@ return {
       vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
         group = lint_augroup,
         callback = function()
-          require('lint').try_lint()
+          local linter = get_linter()
+
+          if linter then
+            lint.linters_by_ft = {
+              typescript = { linter },
+              typescriptreact = { linter },
+            }
+
+            require('lint').try_lint()
+          end
         end,
       })
     end,
