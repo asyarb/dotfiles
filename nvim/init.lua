caa1312aab3f49501bbcc685c1ab6d84ddd118d2
Anthony Yarbrough <asyarb@hawaii.edu>
2024-09-09 15:26:25 -1000

    get rid of modelines

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,6 +12,3 @@ require('keymaps')
 require('autocmd')
 require('lazy-bootstrap')
 require('lazy-plugins')
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
