2e10d9dc95bc2e575e6d01bd208fa3b0126a0fbc
Anthony Yarbrough <asyarb@hawaii.edu>
2025-01-17 09:00:37 -1000

    move rainbow vim opts into config

--- nvim/lua/custom/plugins/rainbow.lua
+++ nvim/lua/custom/plugins/rainbow.lua
@@ -1,13 +1,15 @@
 -- rainbow brackets
 -- https://github.com/HiPhish/rainbow-delimiters.nvim
 
-vim.g.rainbow_delimiters = {
-  query = {
-    javascript = 'rainbow-parens',
-    tsx = 'rainbow-parens',
-  },
-}
-
 return {
   'HiPhish/rainbow-delimiters.nvim',
+  event = 'BufEnter',
+  config = function()
+    vim.g.rainbow_delimiters = {
+      query = {
+        javascript = 'rainbow-parens',
+        tsx = 'rainbow-parens',
+      },
+    }
+  end,
 }
