d84be7cbc1b9772a04a8261208aa08261a82506c
Anthony Yarbrough <asyarb@hawaii.edu>
2025-03-03 14:14:24 -1000

    proper tpope compat

--- nvim/lua/kickstart/plugins/mini.lua
+++ nvim/lua/kickstart/plugins/mini.lua
@@ -1,3 +1,10 @@
+-- Remap adding surrounding to Visual mode selection
+-- vim.keymap.del('x', 'ys')
+vim.keymap.set('x', 'S', [[:<C-u>lua MiniSurround.add('visual')<CR>]], { silent = true })
+
+-- Make special mapping for "add surrounding for line"
+vim.keymap.set('n', 'yss', 'ys_', { remap = true })
+
 return {
   'echasnovski/mini.nvim',
   config = function()
