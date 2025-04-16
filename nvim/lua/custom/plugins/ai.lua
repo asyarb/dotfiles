7a6fc33eb861ad3aa9ec440c2e80a9d275bfdf71
Anthony Yarbrough <asyarb@hawaii.edu>
2025-04-16 10:46:17 -1000

    always use render-markdown

--- nvim/lua/custom/plugins/ai.lua
+++ nvim/lua/custom/plugins/ai.lua
@@ -13,6 +13,12 @@ return {
     {
       'MeanderingProgrammer/render-markdown.nvim',
       ft = { 'markdown', 'codecompanion' },
+      opts = {
+        render_modes = true, -- Render in ALL modes
+        sign = {
+          enabled = false, -- Turn off in the status column
+        },
+      },
     },
   },
   event = 'VeryLazy',
