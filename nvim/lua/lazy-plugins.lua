ab5bd4b5d6a88e0d08471a8702e7ebe1113ae7b2
Anthony Yarbrough <asyarb@hawaii.edu>
2025-06-06 15:58:01 -1000

    lazy lock file

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -57,6 +57,7 @@ require('lazy').setup({
   require('custom.plugins.multicursor'),
   require('custom.plugins.spectre'),
 }, {
+  lockfile = './lazy-lock.json',
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
