c18ce8acc1a15d589b81ad2da14a4765c02dc355
Anthony Yarbrough <asyarb@hawaii.edu>
2026-03-30 13:48:26 -1000

    add my preferred formatters

---
+++ nvim/lua/plugins/formatting.lua
@@ -0,0 +1,37 @@
+return {
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters = {
+				prettierd = {
+					require_cwd = true,
+				},
+				prettierd_svg = {
+					command = "prettierd",
+					require_cwd = true,
+					args = function(_self, ctx)
+						return { ctx.filename, "--parser=html" } -- Note: filename and parser format
+					end,
+				},
+			},
+
+			formatters_by_fmt = {
+				lua = { "stylua" },
+				fish = { "fish_indent" },
+				sh = { "shfmt" },
+
+				markdown = { "oxfmt", "prettierd", stop_after_first = true },
+				javascript = { "oxfmt", "prettierd", stop_after_first = true },
+				typescript = { "oxfmt", "prettierd", stop_after_first = true },
+				typescriptreact = { "oxfmt", "prettierd", stop_after_first = true },
+				json = { "oxfmt", "prettierd", stop_after_first = true },
+				jsonc = { "oxfmt", "prettierd", stop_after_first = true },
+				html = { "oxfmt", "prettierd", stop_after_first = true },
+				svg = { "prettierd_svg" },
+				css = { "oxfmt", "prettierd", stop_after_first = true },
+				php = { "pint" },
+				go = { "gofmt" },
+			},
+		},
+	},
+}
