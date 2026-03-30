773e2f0d33c58b67e05ec2f3df86b538bdacaaf6
Anthony Yarbrough <asyarb@hawaii.edu>
2026-03-30 12:03:52 -1000

    disable inlay hints

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,3 +1,12 @@
 return {
+	-- Disable tabs
 	{ "akinsho/bufferline.nvim", enabled = false },
+
+	-- Disable inlay hints
+	{
+		"nvim-lspconfig",
+		opts = {
+			inlay_hints = { enabled = false },
+		},
+	},
 }
