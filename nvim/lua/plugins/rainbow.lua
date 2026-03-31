b93846bea1b0d95a948c911136c25606b9dce662
Anthony Yarbrough <asyarb@hawaii.edu>
2026-03-30 16:08:53 -1000

    dedent

--- nvim/lua/plugins/rainbow.lua
+++ nvim/lua/plugins/rainbow.lua
@@ -1,14 +1,12 @@
 return {
-	{
-		"HiPhish/rainbow-delimiters.nvim",
-		event = "BufEnter",
-		config = function()
-			vim.g.rainbow_delimiters = {
-				query = {
-					javascript = "rainbow-parens",
-					tsx = "rainbow-parens",
-				},
-			}
-		end,
-	},
+	"HiPhish/rainbow-delimiters.nvim",
+	event = "BufEnter",
+	config = function()
+		vim.g.rainbow_delimiters = {
+			query = {
+				javascript = "rainbow-parens",
+				tsx = "rainbow-parens",
+			},
+		}
+	end,
 }
