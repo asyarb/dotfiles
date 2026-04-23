cf429d262e93f1d1edc19857f6a7162d78a1181c
Anthony Yarbrough <asyarb@hawaii.edu>
2026-04-23 11:08:39 -1000

    remove mini.surround because it sucks

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,14 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Surround keymaps
--- Remap adding surrounding to Visual mode selection
-vim.keymap.del("x", "ys")
-vim.keymap.set("x", "S", [[:<C-u>lua MiniSurround.add('visual')<CR>]], { silent = true })
-
--- Make special mapping for "add surrounding for line"
-vim.keymap.set("n", "yss", "ys_", { remap = true })
-
 -- Tmux Navigator
 vim.keymap.set("n", "<C-h>", "<Cmd>TmuxNavigateLeft<CR>", {})
 vim.keymap.set("n", "<C-j>", "<Cmd>TmuxNavigateDown<CR>", {})
