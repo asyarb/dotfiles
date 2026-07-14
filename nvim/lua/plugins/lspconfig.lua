f7865105ff86286cc23fc3c4d4b3e91fd1d221db
Anthony Yarbrough <asyarb@hawaii.edu>
2026-07-14 09:59:24 -1000

    dont use phpactor

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -2,6 +2,7 @@ return {
 	"neovim/nvim-lspconfig",
 	opts = function(_, opts)
 		opts.diagnostics.virtual_text = false
+		opts.servers.phpactor = false
 		opts.servers.intelephense = {
 			settings = {
 				intelephense = {
