8b0f7f06508b1a7ba7326f5b226409c7fbeaec61
Anthony Yarbrough <asyarb@hawaii.edu>
2026-08-16 09:59:05 -1000

    meh

--- nvim/lua/plugins/surround.lua
+++ nvim/lua/plugins/surround.lua
@@ -1,23 +1,3 @@
 return {
-	"nvim-mini/mini.surround",
-	opts = {
-		mappings = {
-			add = "ys",
-			delete = "ds",
-			find = "",
-			find_left = "",
-			highlight = "",
-			replace = "cs",
-			suffix_last = "",
-			suffix_next = "",
-		},
-		search_method = "cover_or_next",
-	},
-	config = function(_, opts)
-		require("mini.surround").setup(opts)
-
-		vim.keymap.del("x", "ys")
-		vim.keymap.set("x", "S", [[:<C-u>lua MiniSurround.add('visual')<CR>]], { silent = true })
-		vim.keymap.set("n", "yss", "ys_", { remap = true })
-	end,
+	"tpope/vim-surround",
 }
